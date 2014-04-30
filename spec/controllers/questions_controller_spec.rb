ba33ffeae3f2ee23bfa07b976b85dc8c53c17eb9
Bova Bovaev <bova.bovaev@gmail.com>
2014-05-01 00:47:52 +0400

    refactoring: specs for Questions controller; GET #show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -19,12 +19,14 @@ describe QuestionsController do
   describe 'GET #show' do
     let(:question) { create(:question) }
 
-    it 'assign the requested question to @question' do
+    before do
       get :show, id: question
+    end
+    
+    it 'assign the requested question to @question' do
       expect(assigns(:question)).to eq question
     end
     it 'render show view' do
-      get :show, id: question
       expect(response).to render_template :show
     end
   end
