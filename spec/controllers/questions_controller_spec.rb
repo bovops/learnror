49b6a3128feb6cba498cd47c5bd41733bfbb891b
Bova Bovaev <bova.bovaev@gmail.com>
2014-05-01 01:30:30 +0400

    Question specs for create method

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -48,4 +48,26 @@ describe QuestionsController do
     end
   end
 
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new question' do
+        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+      end
+      it 'redirects to show view' do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'question not saved' do
+        expect { post :create, question: attributes_for(:invalid_question) }.to_not change(Question, :count)
+      end
+
+      it 're-render new view' do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
