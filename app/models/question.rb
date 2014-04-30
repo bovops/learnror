83d91ade022d12637e38c4f0c2359caa0ace40f4
Bova Bovaev <bova.bovaev@gmail.com>
2014-04-30 19:05:30 +0400

    Question model refactoring

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,4 @@
 class Question < ActiveRecord::Base
-	validates_presence_of 	:title
-	validates_presence_of 	:body
+	validates :title, :body, presence: true
 	validates_length_of		:title, in: 5..255
 end
