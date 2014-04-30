ed79160199c376cf8615200875c8e2f68216c981
Bova Bovaev <bova.bovaev@gmail.com>
2014-05-01 00:46:37 +0400

    specs for Questions controller; GET #show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,4 +16,17 @@ describe QuestionsController do
     end
   end
 
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    it 'assign the requested question to @question' do
+      get :show, id: question
+      expect(assigns(:question)).to eq question
+    end
+    it 'render show view' do
+      get :show, id: question
+      expect(response).to render_template :show
+    end
+  end
+
 end
