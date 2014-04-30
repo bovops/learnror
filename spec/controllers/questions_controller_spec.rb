fe85e461652ed8cbee2713fd3ce26bc21bbf4aef
Bova Bovaev <bova.bovaev@gmail.com>
2014-05-01 00:35:36 +0400

    specs for Questions controller; GET #index

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe QuestionsController do
+  describe "GET #index" do
+    it 'assigns all questions to array' do
+      questions = create_list(:question, 5)
+      get :index
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'render index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 
 end
