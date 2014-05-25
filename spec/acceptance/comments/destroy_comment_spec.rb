6271fd7e5ac2aa3f68eb4099edf484c279b73315
Bova Bovaev <ink08@ink-online.ru>
2014-05-25 13:47:37 +0400

    [71994252] Authenticated user can destroy his comment

---
+++ spec/acceptance/comments/destroy_comment_spec.rb
@@ -0,0 +1,54 @@
+require_relative '../acceptance_helper'
+
+feature 'Delete comment', %q{
+  In order to fix mistake
+  As an author of comment
+  I'd like to destroy my comment
+} do
+
+  given!(:user) { create(:user) }
+  given!(:other_user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, question: question, user: user) }
+  given!(:comment) { create(:comment, commentable: answer, user: user) }
+  given!(:other_comment) { create(:comment, commentable: answer, user: other_user) }
+
+  scenario 'Unauthenticated user try to destroy comment' do
+    visit question_path(question)
+
+    within "#comment_#{comment.id}" do
+      expect(page).to_not have_link 'Delete'
+    end
+  end
+
+  describe 'Authenticated user' do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'Sees link to destroy comment' do
+      within "#comment_#{comment.id}" do
+        expect(page).to have_link 'Delete'
+      end
+    end
+
+    scenario 'try to destroy his comment', js: true do
+      within "#comment_#{comment.id}" do
+        click_on 'Delete'
+      end
+
+      expect(current_path).to eq question_path(question)
+      expect(page).to_not have_selector "#comment_#{comment.id}"
+    end
+
+    scenario 'try to destroy not-owned comment', js: true do
+      within "#comment_#{other_comment.id}" do
+        expect(page).to_not have_link 'Delete'
+      end
+    end
+
+  end
+
+
+end
